66f79dd167854d84454f8c612002bd3b865dc28b
Thaddeus McCleary <thaddeusmccleary@Thaddeus-Computer.local>
2019-04-27 14:16:56 +0800

    Remove helmet

--- index.js
+++ index.js
@@ -1,13 +1,8 @@
 const express = require('express')
-const helmet = require('helmet')
 const app = express()
 
 var http = require('http').Server(app)
 var io = require('socket.io')(http)
-
-
-// add some security-related headers to the response
-app.use(helmet())
 
 io.on('connection', function(socket) {
 	console.log('connected', socket.id)
