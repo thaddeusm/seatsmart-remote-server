6e288000e6530767aea59f035da2e515650e6b91
Thaddeus McCleary <thaddeusmccleary@Thaddeus-Computer.local>
2019-04-27 16:24:43 +0800

    Start host socket connection configuration

--- index.js
+++ index.js
@@ -2,10 +2,38 @@ const app = require('express')()
 const server = require('http').Server(app)
 const io = require('socket.io')(server)
 
+const simpleId = require('simple-id')
+
+var rooms = []
+
 io.on('connection', socket => {
   console.log('a user connected')
+
+  socket.on('action', (action) => {
+  	console.log(action)
+  })
+
+  // set up room to secure messages
+  socket.on('establishRoom', (passphrase) => {
+  	let newId = simpleId()
+
+  	socket.join(newId)
+
+  	let roomInfo = {
+  		id: newId,
+  		passphrase: passphrase,
+  		host: socket.id,
+  		remote: ''
+  	}
+
+  	rooms.push(roomInfo)
+
+  	io.to(newId).emit('roomEstablished', newId)
+  	io.to(socket.id).emit('remoteConnected', roomInfo)
+  })
+
 })
 
 server.listen(4000, () => {
-  console.log('The server is running: http://localhost:4000')
+  console.log('The server is running')
 })
