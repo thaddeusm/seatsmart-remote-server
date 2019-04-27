5fe104a2f9397f4f1cfa6f5d6cf1643d5b96ae12
Thaddeus McCleary <thaddeusmccleary@Thaddeus-Computer.local>
2019-04-27 14:34:41 +0800

    Debug configuration

--- index.js
+++ index.js
@@ -1,8 +1,9 @@
-const express = require('express')
-const app = express()
+var express = require('express')
+  , http = require('http')
 
-var http = require('http').Server(app)
-var io = require('socket.io')(http)
+var app = express()
+var server = http.createServer(app)
+var io = require('socket.io').listen(server)
 
 io.on('connection', function(socket) {
 	console.log('connected', socket.id)
