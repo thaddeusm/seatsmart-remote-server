f653fdc73cc5123a612346de2694022278036c1f
Thaddeus McCleary <thaddeusmccleary@Thaddeus-Computer.local>
2019-04-27 13:03:10 +0800

    added socket

--- index.js
+++ index.js
@@ -1,17 +1,16 @@
 const express = require('express')
 const helmet = require('helmet')
-
 const app = express()
+
+var http = require('http').Server(app)
+var io = require('socket.io')(http)
+
 
 // add some security-related headers to the response
 app.use(helmet())
 
-app.get('*', (req, res) => {
-    res.set('Content-Type', 'text/html')
-    res.status(200).send(`
-        <h1><marquee direction=right>Hello from Express path '/' on Now 2.0!</marquee></h1>
-        <h2>Go to <a href="/about">/about</a></h2>
-    `)
+io.on('connection', function(socket) {
+	console.log('connected', socket.id)
 })
 
 module.exports = app
