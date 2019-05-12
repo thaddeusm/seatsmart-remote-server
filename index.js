a58279d06c4fa869cfc0f1218d11c919715a6d73
Thaddeus McCleary <thaddeusmccleary@Thaddeus-Computer.local>
2019-05-12 15:41:16 +0800

    Add action handling

--- index.js
+++ index.js
@@ -53,6 +53,16 @@ io.on('connection', socket => {
     io.to(idDictionary[socket.id]).emit('incomingData', data)
   })
 
+  // client initiates action
+  socket.on('initAction', (action) => {
+    io.to(idDictionary[socket.id]).emit('requestAction', action)
+  })
+
+  // host responds with action completion confirmation
+  socket.on('initConfirm', (actionID) => {
+    io.to(idDictionary[socket.id]).emit('confirmAction', actionID)
+  })
+
   // reconnect
   socket.on('rejoinRoom', (room) => {
     socket.join(room)
