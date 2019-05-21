6a54cffd5ece7fb9649aad7fff8bdfcff180a91b
Thaddeus McCleary <thaddeusmccleary@Thaddeus-Computer.local>
2019-05-21 21:07:07 +0800

    Remove unnecessary code and add comments

--- index.js
+++ index.js
@@ -4,20 +4,17 @@ const io = require('socket.io')(server)
 
 const simpleId = require('simple-id')
 
+// this object holds information about connected devices and rooms
 var idDictionary = {}
 
 io.on('connection', socket => {
   console.log('a user connected')
 
-  socket.on('action', (action) => {
-  	console.log(action)
-  })
-
   // set up room to secure messages
   socket.on('establishRoom', () => {
   	let newId = simpleId()
 
-    // sent host to newly created room
+    // send host to newly created room
   	socket.join(newId)
 
     // if the host is starting a new connection, close former
@@ -39,6 +36,7 @@ io.on('connection', socket => {
     // register client in dictionary
     idDictionary[socket.id] = room
 
+    // send notification to remote device and host
     io.to(socket.id).emit('roomJoined', room)
     io.to(room).emit('remoteConnected')
   })
@@ -70,16 +68,21 @@ io.on('connection', socket => {
     // reregister device in dictionary in case of id change
     idDictionary[socket.id] = room
 
+    // send notification to room
     io.to(idDictionary[socket.id]).emit('rejoinedRoom')
   })
 
   // host ends session
   socket.on('endSession', () => {
+    // notify remote
     io.to(idDictionary[socket.id]).emit('sessionEnded')
+
+    // remove device from room
     socket.leave(idDictionary[socket.id])
   })
 
   socket.on('disconnect', () => {
+    // send notification to room
     io.to(idDictionary[socket.id]).emit('deviceDisconnection')
   })
 
